f5a3ef1b9261b940cf63a5503b7d3d79f1a325d9
reem <ryma6110@gmail.com>
2026-04-29 15:52:48 +0000

    Add task 28: bst_search

--- 113-bst_search.c
+++ 113-bst_search.c
@@ -5,21 +5,19 @@
  * @tree: Pointer to the root node of the BST to search
  * @value: The value to search in the tree
  *
- * Return: A pointer to the node containing the value, or NULL if not found
+ * Return: A pointer to the node containing a value equals to value,
+ * or NULL if tree is NULL or nothing is found
  */
 bst_t *bst_search(const bst_t *tree, int value)
 {
-	bst_t *node = (bst_t *)tree;
+	if (tree == NULL)
+		return (NULL);
 
-	while (node != NULL)
-	{
-		if (value == node->n)
-			return (node);
-		if (value < node->n)
-			node = node->left;
-		else
-			node = node->right;
-	}
+	if (value == tree->n)
+		return ((bst_t *)tree);
 
-	return (NULL);
+	if (value < tree->n)
+		return (bst_search(tree->left, value));
+
+	return (bst_search(tree->right, value));
 }
--- binary_trees.h
+++ binary_trees.h
@@ -3,39 +3,22 @@
 
 #include <stddef.h>
 #include <stdlib.h>
-#include <limits.h>
 
-/**
- * struct binary_tree_s - Binary tree node
- * @n: Integer stored in the node
- * @parent: Pointer to the parent node
- * @left: Pointer to the left child node
- * @right: Pointer to the right child node
- */
+/* Data Structure */
 struct binary_tree_s
 {
-	int n;
-	struct binary_tree_s *parent;
-	struct binary_tree_s *left;
-	struct binary_tree_s *right;
+    int n;
+    struct binary_tree_s *parent;
+    struct binary_tree_s *left;
+    struct binary_tree_s *right;
 };
-
 typedef struct binary_tree_s binary_tree_t;
 typedef struct binary_tree_s bst_t;
-typedef struct binary_tree_s avl_t;
-typedef struct binary_tree_s heap_t;
-
-/* Helper */
-void binary_tree_print(const binary_tree_t *);
 
 /* Prototypes */
+void binary_tree_print(const binary_tree_t *);
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
-int binary_tree_is_avl(const binary_tree_t *tree);
-int binary_tree_balance(const binary_tree_t *tree);
-binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree);
-binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree);
-
-/* AVL specific */
-avl_t *avl_insert(avl_t **tree, int value);
+bst_t *array_to_bst(int *array, size_t size);
+bst_t *bst_search(const bst_t *tree, int value);
 
 #endif /* BINARY_TREES_H */
