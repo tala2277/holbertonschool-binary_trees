f3389a5e41c5b45274ccecd68756032ab93b5326
reem <ryma6110@gmail.com>
2026-04-29 16:04:16 +0000

    Add task 39: array_to_heap

--- binary_trees.h
+++ binary_trees.h
@@ -20,5 +20,6 @@ void binary_tree_print(const binary_tree_t *);
 /* Prototypes */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 heap_t *heap_insert(heap_t **root, int value);
+heap_t *array_to_heap(int *array, size_t size);
 
 #endif /* BINARY_TREES_H */
