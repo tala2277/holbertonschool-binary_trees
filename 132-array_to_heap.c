291efd17d09952d996f0aa811576376843a48c36
reem <ryma6110@gmail.com>
2026-04-29 16:05:07 +0000

    Fix Betty style: line over 80 characters in documentation

--- 132-array_to_heap.c
+++ 132-array_to_heap.c
@@ -5,7 +5,7 @@
  * @array: Pointer to the first element of the array to be converted
  * @size: Number of elements in the array
  *
- * Return: Pointer to the root node of the created Binary Heap, or NULL on failure
+ * Return: Pointer to the root node of the created Binary Heap, or NULL
  */
 heap_t *array_to_heap(int *array, size_t size)
 {
