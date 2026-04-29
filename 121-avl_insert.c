d3e0870bc0f8e8593c6fa50384c4a8a408dac314
reem <ryma6110@gmail.com>
2026-04-29 15:51:23 +0000

    Fix Betty style: split functions and wrap long lines

--- 121-avl_insert.c
+++ 121-avl_insert.c
@@ -1,6 +1,34 @@
 #include "binary_trees.h"
 
 /**
+ * balance_avl - Balances an AVL tree after insertion
+ * @tree: Double pointer to the node to balance
+ * @value: Value inserted
+ * Return: Pointer to the new root of the subtree
+ */
+avl_t *balance_avl(avl_t **tree, int value)
+{
+	int balance;
+
+	balance = binary_tree_balance(*tree);
+	if (balance > 1 && value < (*tree)->left->n)
+		return (binary_tree_rotate_right(*tree));
+	if (balance < -1 && value > (*tree)->right->n)
+		return (binary_tree_rotate_left(*tree));
+	if (balance > 1 && value > (*tree)->left->n)
+	{
+		(*tree)->left = binary_tree_rotate_left((*tree)->left);
+		return (binary_tree_rotate_right(*tree));
+	}
+	if (balance < -1 && value < (*tree)->right->n)
+	{
+		(*tree)->right = binary_tree_rotate_right((*tree)->right);
+		return (binary_tree_rotate_left(*tree));
+	}
+	return (*tree);
+}
+
+/**
  * avl_insert_recursive - Inserts a value into an AVL tree recursively
  * @tree: Double pointer to the current node
  * @parent: Parent of the current node
@@ -11,8 +39,6 @@
 avl_t *avl_insert_recursive(avl_t **tree, avl_t *parent,
 			    avl_t **new_node, int value)
 {
-	int balance;
-
 	if (*tree == NULL)
 	{
 		*new_node = binary_tree_node(parent, value);
@@ -21,40 +47,22 @@ avl_t *avl_insert_recursive(avl_t **tree, avl_t *parent,
 
 	if (value < (*tree)->n)
 	{
-		(*tree)->left = avl_insert_recursive(&((*tree)->left), *tree, new_node, value);
+		(*tree)->left = avl_insert_recursive(&((*tree)->left),
+						     *tree, new_node, value);
 		if (!*new_node)
 			return (NULL);
 	}
 	else if (value > (*tree)->n)
 	{
-		(*tree)->right = avl_insert_recursive(&((*tree)->right), *tree, new_node, value);
+		(*tree)->right = avl_insert_recursive(&((*tree)->right),
+						      *tree, new_node, value);
 		if (!*new_node)
 			return (NULL);
 	}
 	else
 		return (*tree);
 
-	balance = binary_tree_balance(*tree);
-
-	if (balance > 1 && value < (*tree)->left->n)
-		return (binary_tree_rotate_right(*tree));
-
-	if (balance < -1 && value > (*tree)->right->n)
-		return (binary_tree_rotate_left(*tree));
-
-	if (balance > 1 && value > (*tree)->left->n)
-	{
-		(*tree)->left = binary_tree_rotate_left((*tree)->left);
-		return (binary_tree_rotate_right(*tree));
-	}
-
-	if (balance < -1 && value < (*tree)->right->n)
-	{
-		(*tree)->right = binary_tree_rotate_right((*tree)->right);
-		return (binary_tree_rotate_left(*tree));
-	}
-
-	return (*tree);
+	return (balance_avl(tree, value));
 }
 
 /**
