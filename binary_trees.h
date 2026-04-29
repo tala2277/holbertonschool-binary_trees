6ce6b489c221070678d80064e044c17e38925895
reem <ryma6110@gmail.com>
2026-04-29 15:53:55 +0000

    Add task 33: array_to_avl

--- binary_trees.h
+++ binary_trees.h
@@ -4,21 +4,23 @@
 #include <stddef.h>
 #include <stdlib.h>
 
-/* Data Structure */
+/* Structures */
 struct binary_tree_s
 {
-    int n;
-    struct binary_tree_s *parent;
-    struct binary_tree_s *left;
-    struct binary_tree_s *right;
+	int n;
+	struct binary_tree_s *parent;
+	struct binary_tree_s *left;
+	struct binary_tree_s *right;
 };
 typedef struct binary_tree_s binary_tree_t;
-typedef struct binary_tree_s bst_t;
+typedef struct binary_tree_s avl_t;
 
-/* Prototypes */
+/* Helper */
 void binary_tree_print(const binary_tree_t *);
+
+/* Required Prototypes */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
-bst_t *array_to_bst(int *array, size_t size);
-bst_t *bst_search(const bst_t *tree, int value);
+avl_t *avl_insert(avl_t **tree, int value);
+avl_t *array_to_avl(int *array, size_t size);
 
 #endif /* BINARY_TREES_H */
