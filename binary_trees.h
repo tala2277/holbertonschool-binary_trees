5c5a5ba6d28e6b1c12032c46483f61fe0dce11c7
reem <ryma6110@gmail.com>
2026-04-29 15:11:10 +0000

    Add prototype for task 100

--- binary_trees.h
+++ binary_trees.h
@@ -30,5 +30,6 @@ void binary_tree_print(const binary_tree_t *);
 /* Task Prototypes */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second);
 
 #endif /* BINARY_TREES_H */
