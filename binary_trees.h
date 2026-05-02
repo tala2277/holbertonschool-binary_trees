8061f6f5703c78f04e1d2b1cb953c055a869a22c
tala2277 <tt1341700@gmail.com>
2026-05-02 16:37:37 +0000

    Add heap sort prototypes

--- binary_trees.h
+++ binary_trees.h
@@ -23,4 +23,7 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 heap_t *array_to_heap(int *array, size_t size);
 int heap_extract(heap_t **root);
 
+int heap_extract(heap_t **root);
+int *heap_to_sorted_array(heap_t *heap, size_t *size);
+
 #endif /* BINARY_TREES_H */
