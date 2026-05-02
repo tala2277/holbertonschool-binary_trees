cc6ba52f415ed5435749107d9a5879d53b75bbe4
tala2277 <tt1341700@gmail.com>
2026-05-02 16:36:47 +0000

    Add heap to sorted array

--- 134-heap_to_sorted_array.c
+++ 134-heap_to_sorted_array.c
@@ -12,21 +12,13 @@ int *heap_to_sorted_array(heap_t *heap, size_t *size)
 {
 	int *array;
 	size_t i = 0;
-	size_t s = 0;
-	heap_t *tmp = heap;
 
-	/* calculate size */
-	while (tmp)
+	if (!heap)
 	{
-		s++;
-		tmp = tmp->left;
+		*size = 0;
+		return (NULL);
 	}
 
-	*size = 0;
-	if (!heap)
-		return (NULL);
-
-	/* safer: use binary_tree_size if عندكم */
 	*size = binary_tree_size(heap);
 
 	array = malloc(sizeof(int) * (*size));
