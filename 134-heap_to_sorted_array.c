f1477610e0b842b526363b3c61dc7fb053d9ae46
tala2277 <tt1341700@gmail.com>
2026-05-02 16:38:08 +0000

    Fix heap sort size helper

--- 134-heap_to_sorted_array.c
+++ 134-heap_to_sorted_array.c
@@ -2,33 +2,45 @@
 #include "binary_trees.h"
 
 /**
- * heap_to_sorted_array - converts heap to sorted array (descending)
- * @heap: pointer to root
- * @size: address to store size
+ * tree_size - measures the size of a binary tree
+ * @tree: pointer to root node
  *
- * Return: pointer to array or NULL
+ * Return: size of tree
+ */
+static size_t tree_size(const heap_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+
+	return (1 + tree_size(tree->left) + tree_size(tree->right));
+}
+
+/**
+ * heap_to_sorted_array - converts a max heap to a sorted array
+ * @heap: pointer to root node
+ * @size: address to store array size
+ *
+ * Return: sorted array in descending order, or NULL
  */
 int *heap_to_sorted_array(heap_t *heap, size_t *size)
 {
 	int *array;
-	size_t i = 0;
+	size_t i;
 
-	if (!heap)
+	if (heap == NULL)
 	{
 		*size = 0;
 		return (NULL);
 	}
 
-	*size = binary_tree_size(heap);
-
+	*size = tree_size(heap);
 	array = malloc(sizeof(int) * (*size));
-	if (!array)
+	if (array == NULL)
 		return (NULL);
 
-	while (heap)
-	{
+	i = 0;
+	while (heap != NULL)
 		array[i++] = heap_extract(&heap);
-	}
 
 	return (array);
 }
